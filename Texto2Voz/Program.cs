633ee2debc69fb424c0805d80088b2ea9ecaf01d
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2020-11-12 11:44:11 -0600

    Add files via upload

---
+++ Texto2Voz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texto2Voz{
+    class Program{
+        static void Main(string[] args){
+            Voz Sonido = new Voz("Hola Mundo");
+            bool Ejecucion = Sonido.Hablar();
+
+            Console.WriteLine("Ejecucion exitosa? {0}", Ejecucion);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Texto2Voz/Voz.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texto2Voz{
+    class Voz{
+        string pronunciar = "";
+        public Voz(string texto) {
+            pronunciar = texto;
+        }
+        public bool Hablar() {
+            try {
+                SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+                synthesizer.Volume = 100;  // 0...100
+                synthesizer.Rate = -2;     // -10...10
+                synthesizer.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Senior);
+                synthesizer.Speak(pronunciar); //synthesizer.SpeakAsync(texto);
+                return true;
+            }
+            catch {
+                return false;
+            }
+        }
+
+    }
+}
